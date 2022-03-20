8ee7c95c30c90666acca76b455ee60730b39f6a4
Haci Murat Yaman <haci@muratyaman.co.uk>
2022-03-20 10:28:58 +0000

    use latest pgsqlinfo, minor update

--- src/files.ts
+++ src/files.ts
@@ -1,7 +1,7 @@
 import fse from 'fs-extra';
 import { resolve } from 'path';
 
-export function writeSchemaFile(outDir: string, schemaName: string, content: string): void {
+export function writeSchemaFileSync(outDir: string, schemaName: string, content: string): void {
   const filePath = resolve(outDir, `${schemaName}.ts`);
   // create if it does not exist
   fse.ensureFile(filePath);
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,59 @@
 import dotenv from 'dotenv';
+import { Pool, PoolConfig } from 'pg';
 import { PgInfoService } from 'pgsqlinfo';
 import { makeCommand } from './cmd';
-import { writeSchemaFile } from './files';
+import { writeSchemaFileSync } from './files';
 import { PgDtoGenerator } from './PgDtoGenerator';
 
 dotenv.config();
 main();
 
 async function main() {
-  const cmd = makeCommand();
-
-  cmd.parse(process.argv);
-  const options = cmd.opts();
-
-  const dbOptions = {
-    host: options.host,
-    port: options.port,
-    user: options.user,
-    password: options.password,
-    database: options.database,
-  };
-
-  const outDir = options.outdir;
-
-  const dbInfo = new PgInfoService(dbOptions, dbOptions.database);
-  // const one = await dbInfo.query('SELECT 1 AS one');
-
-  const generator = new PgDtoGenerator(dbInfo);
-
+  let exitCode = 0;
   console.info('begin!');
-  const schemaOutputList = await generator.generate();
-  for (const schemaOutput of schemaOutputList) {
-    writeSchemaFile(outDir, schemaOutput.schemaName, schemaOutput.content);
+
+  try {
+    const cmd = makeCommand();
+
+    cmd.parse(process.argv);
+    const options = cmd.opts();
+
+    const dbOptions: PoolConfig = {
+      host: options.host ?? 'localhost',
+      port: options.port ?? '5432',
+      user: options.user ?? '',
+      password: options.password ?? '',
+      database: options.database ?? '',
+    };
+
+    const outDir = String(options.outdir ?? '').trim();
+    if (outDir === '') throw new Error('Output directory is required');
+
+    const dbPool = new Pool(dbOptions);
+    try {
+      console.info('checking connection to your database...');
+      const success = await dbPool.query('SELECT 1 AS success');
+      console.info('checking connection to your database... done!', success.rows);
+      const dbInfo = new PgInfoService(dbPool, dbOptions.database ?? '');
+
+      const generator = new PgDtoGenerator(dbInfo);
+
+      const schemaOutputList = await generator.generate();
+      for (const schemaOutput of schemaOutputList) {
+        writeSchemaFileSync(outDir, schemaOutput.schemaName, schemaOutput.content);
+      }
+    } catch (err) {
+      exitCode = -1;
+      console.error(err);
+    } finally {
+      dbPool.end();
+    }
+
+  } catch (err) {
+    exitCode = -1;
+    console.error(err);
   }
+
   console.info('the end!');
-  process.exit(0);
+  process.exit(exitCode);
 }
